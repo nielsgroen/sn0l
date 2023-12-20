c20678adf25dff18c9e74089aba54e2b4df2e55b
Niels Groeneveld <groeneveld@salure.nl>
2023-12-20 20:22:12 +0100

    main.rs cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,17 @@ use std::io::{BufRead};
 use std::thread;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use anyhow;
-use chess::{Board};
-use clap::Parser;
-use sn0l::core::search::iterative_deepening::iterative_deepening_search;
-use sn0l::core::search::search_result::minimal_search_result::MinimalSearchResult;
 use sn0l::core::search::SearchCommand;
-use sn0l::core::search::transpositions::no_transposition::NoTranspositionTable;
-use sn0l::core::search::transpositions::TranspositionTable;
-use sn0l::input;
-use sn0l::input::protocol_interpreter::{CalculateOptions, Command};
+use sn0l::input::protocol_interpreter::Command;
 use sn0l::input::stdin::listen_to_stdin;
 
 
 fn main() -> anyhow::Result<()> {
-    let cli = input::command_line::Cli::parse();
-
-    println!("{:?}", cli);
-
-    match cli.benchmark {
-        true => run_benchmark(),
-        false => start_uci_protocol(),
-    }
-
-
+    start_uci_protocol()
 }
 
 fn start_uci_protocol() -> anyhow::Result<()> {
     // Make sure host (GUI) uses UCI protocol
-    println!("started!");
     loop {
         let mut buffer = String::new();
         io::stdin().lock().read_line(&mut buffer).expect("Failed stdin read");
@@ -42,7 +25,6 @@ fn start_uci_protocol() -> anyhow::Result<()> {
             return Ok(());
         }
 
-        break; // TODO: remove
         // run again: GUI may have tried some other protocol, e.g. `xboard`
     }
 
@@ -52,7 +34,6 @@ fn start_uci_protocol() -> anyhow::Result<()> {
     let (search_tx, search_rx) = channel::<SearchCommand>();
 
     pre_option_init(input_tx, search_rx);
-    // let mut current_board = Board::default();  // TODO move this to the core engine
     println!("uciok"); // confirm pre-init
 
     loop {
@@ -64,38 +45,13 @@ fn start_uci_protocol() -> anyhow::Result<()> {
 
         match command {
             Command::IsReady => println!("readyok"),  // Main thread unblocked, so must be ready
-            Command::Quit => break,
-            // Command::SetPosition(board) => current_board = board,
-            // Command::Calculate(_) => {
-            //     // let mut candidate_moves = MoveGen::new_legal(&current_board);
-            //     // let chosen_move = candidate_moves.next().unwrap();
-            //     let chosen_move = core::evaluation_old::best_move_depth(&current_board, 5).unwrap();
-            //     println!("bestmove {}", chosen_move)
-            // }
-            _ => (),  // TODO
+            Command::Quit => break,  // TODO: should interrupt search, instead of letting it finish
+            _ => (),  // currently unsupported command: Ignore, may have supported new protocols
         }
     }
 
     return Ok(());
 }
-
-fn run_benchmark() -> anyhow::Result<()> {
-    println!("Started benchmark");
-    let mut transposition_table: Box<dyn TranspositionTable> = Box::new(NoTranspositionTable::default());
-
-    let (result, _depth, _selective_depth): (MinimalSearchResult, _, _) =
-        iterative_deepening_search(
-            &Board::default(),
-            &mut transposition_table,
-            Vec::new(),
-            CalculateOptions::Depth(5),
-            |_, _| {},
-        );
-
-    println!("Done");
-    Ok(())
-}
-
 
 fn pre_option_init(input_tx: Sender<Command>, search_rx: Receiver<SearchCommand>) {
     // The thread that listens to stdin
